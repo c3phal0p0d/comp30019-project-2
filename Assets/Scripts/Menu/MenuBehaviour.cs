3f2596066892c6cb4009851f005a8af8bbde973b
Natasha Chiorsac <75411738+natashachiorsac@users.noreply.github.com>
2022-10-26 17:22:30 +1100

    moved StartMenu to StartScene

--- Assets/Scripts/Menu/MenuBehaviour.cs
+++ Assets/Scripts/Menu/MenuBehaviour.cs
@@ -61,8 +61,12 @@ public class MenuBehaviour : MonoBehaviour
 
     public void QuitGame()
     {
-
-        Application.Quit();
+        #if UNITY_STANDALONE
+            Application.Quit();
+        #endif
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #endif
     }
 
 }
